025075b3bc9ab060fed28913d850ca71907b674b
tejudeokar22 <tejudeokar22@gmail.com>
2022-04-17 12:30:04 +0530

    this is pom class for students

---
+++ Zerodha001/Pomclass.java
@@ -0,0 +1,69 @@
+package Pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+public class Pomclass {
+	@FindBy(xpath="//input[@name= 'firstname']")private WebElement Fname;
+	  @FindBy(xpath="//input[@name= 'lastname']")private WebElement Lname;
+	  @FindBy(xpath="//input[@name='reg_email__']")private WebElement Email;
+	  @FindBy(xpath="//input[@data-type='password']")private WebElement Password;
+	  @FindBy(xpath="//button[@type='submit']")private WebElement Logbtn;
+	  @FindBy(xpath="//select[@title='Month']")private WebElement Month;
+	  @FindBy(xpath="//select[@title='Day']")private WebElement Day;
+	  @FindBy(xpath="//select[@title='Year']")private WebElement Year;
+
+		public Pomclass(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+	}
+		void Pomclass(WebDriver ar)
+		 {
+			PageFactory.initElements(ar, this);
+		 }
+	  public void Enterfname(String fname) 
+	  {
+		Fname.sendKeys(fname);
+	  }	
+		
+	 public void EnterLname(String lname) 
+	 {
+		Lname.sendKeys(lname);
+	 } 
+	 public void EnterEmail(String email) 
+	  	{
+		 Email.sendKeys(email);
+		 
+	  	}
+	 	public void Enterpassword(String pass) 
+	 	{
+		 Password.sendKeys(pass);
+	  	}
+	 	public void clicklogin() 
+	 		{
+	 			Logbtn.click();
+	 		}
+	 	public void selectday(String day ) 
+	 		{
+	 			Select sel = new Select(Day);
+	 			sel.selectByValue(day);
+	 			
+	 		}
+	 	
+	 	  public void selectMonth(String mon) 
+	 			{
+	 				Select ss = new Select(Month);
+	 				ss.selectByValue(mon);
+	 			} 
+	 	  public void selectYear(String yo) 
+	 			{
+	 				Select ss = new Select(Year);
+	 				ss.selectByValue(yo);
+	 			}
+	 	
+	 	
+	 }
+
+
